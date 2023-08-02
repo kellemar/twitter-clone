a9053928f0ae7b741be41b8109222db1a9e29dcc
Alvin De Cruz <alvin.decruz@avocadoguild.com>
2023-08-02 17:08:26 +0800

    removed red background on new tweet box

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -1,6 +1,7 @@
 import { User, createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Image from "next/image";
+
 export const dynamic = "force-dynamic";
 export default function NewTweet( {user}: {user: User}){
     const addTweet = async (formData: FormData) => {
@@ -16,7 +17,7 @@ export default function NewTweet( {user}: {user: User}){
     return (
         <form className="border border-gray-800 border-t-0" action={addTweet}>
             <div className="flex px-4 py-8">
-            <div className="h-12 w-12 bg-red-200"><Image className="rounded-full" src={user.user_metadata.avatar_url} alt="user avatar" width={48} height={48}/></div>
+            <div className="h-12 w-12"><Image className="rounded-full" src={user.user_metadata.avatar_url} alt="user avatar" width={48} height={48}/></div>
             <input  name="title" placeholder="What's happening?" className="bg-inherit flex-1 ml-2 text-2xl leading-loose placeholder-gray-500 px-2"  />
             </div>
 
